7d83b7e7f450fe7d25f94632587e279e5f97d50b
Fernando Crespo <fernando82@gmail.com>
2017-03-29 23:31:55 -0300

    Remove old code

--- test.py
+++
@@ -1,8 +0,0 @@
-#!./env/bin/python
-
-import name_strategy.exif
-import name_strategy.filename
-
-print(name_strategy.exif.kwargs_from_image('./teste/IMG_0357.JPG'))
-
-print(name_strategy.filename.kwargs_from_image('./teste/IMG_22.10.2016.10.10.10.JPG'))
